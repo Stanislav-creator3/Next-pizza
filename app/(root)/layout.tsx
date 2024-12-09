1ed03779fff531cb00a62edce618e947d7423a58
Stas00001 <116278430+Stas00001@users.noreply.github.com>
2024-12-09 18:49:33 +0300

    fix: error client

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import { Header } from "@/shared/components/shared/header";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Next Pizza | Главная",
@@ -8,14 +9,16 @@ export const metadata: Metadata = {
 
 export default function HomeLayout({
   children,
-  modal
+  modal,
 }: Readonly<{
   children: React.ReactNode;
   modal: React.ReactNode;
 }>) {
   return (
     <main className="min-h-screen">
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       {children}
       {modal}
     </main>
